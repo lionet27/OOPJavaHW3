dbbe759aa35883d65779451aeb228061f0454b61
«OlgaSoboleva27» <osoboleva27@gmail.com>
2023-03-12 00:27:02 +0300

    add Component

---
+++ Java/Lesson3/drugs/Component.java
@@ -0,0 +1,81 @@
+package Lesson3.drugs;
+
+public class Component {
+    private String name;
+    private String weight;
+    private int power;
+
+    public Component(String name, String weight, int power) {
+        this.name = name;
+        this.weight = weight;
+        this.power = power;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    @Override
+    public String toString() {
+        return "Component{" +
+                "name='" + name + '\'' +
+                ", weight='" + weight + '\'' +
+                ", power=" + power +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((weight == null) ? 0 : weight.hashCode());
+        result = prime * result + power;
+        
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Component other = (Component) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (weight == null) {
+            if (other.weight != null)
+                return false;
+        } else if (!weight.equals(other.weight))
+            return false;
+        if (power != other.power)
+            return false;
+        return true;
+    }
+}
