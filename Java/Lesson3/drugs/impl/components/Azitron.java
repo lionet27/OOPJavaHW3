19395b133206532cbf8b6de1155c41b5a6c60c74
«OlgaSoboleva27» <osoboleva27@gmail.com>
2023-03-12 00:28:27 +0300

    add Azitron

---
+++ Java/Lesson3/drugs/impl/components/Azitron.java
@@ -0,0 +1,9 @@
+package Lesson3.drugs.impl.components;
+
+import Lesson3.drugs.Component;
+
+public class Azitron extends Component {
+    public Azitron(String name, String weight, int power) {
+        super(name, weight, power);
+    }
+}
