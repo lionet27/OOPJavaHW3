504c0ab9bc55d5ab4590d3c1fc69c2ee5a975c33
«OlgaSoboleva27» <osoboleva27@gmail.com>
2023-03-12 00:27:46 +0300

    add CatDrug

---
+++ Java/Lesson3/drugs/impl/CatDrug.java
@@ -0,0 +1,12 @@
+package Lesson3.drugs.impl;
+
+import Lesson3.drugs.Component;
+import Lesson3.drugs.Drug;
+
+import java.util.List;
+
+public class CatDrug extends Drug {
+    public CatDrug(String name, List<Component> component) {
+        super(name, component);
+    }
+}
