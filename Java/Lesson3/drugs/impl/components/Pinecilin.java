66a0736efcb5b41471f04e09673f465fc7f72ec2
«OlgaSoboleva27» <osoboleva27@gmail.com>
2023-03-12 00:29:01 +0300

    add Pinecilin

---
+++ Java/Lesson3/drugs/impl/components/Pinecilin.java
@@ -0,0 +1,9 @@
+package Lesson3.drugs.impl.components;
+
+import Lesson3.drugs.Component;
+
+public class Pinecilin extends Component {
+    public Pinecilin(String name, String weight, int power) {
+        super(name, weight, power);
+    }
+}
