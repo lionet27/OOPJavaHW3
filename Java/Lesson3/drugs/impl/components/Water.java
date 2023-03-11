13a3862d3e23c7ac77fbe6dfb52375702eb70f7a
«OlgaSoboleva27» <osoboleva27@gmail.com>
2023-03-12 00:29:26 +0300

    add Water

---
+++ Java/Lesson3/drugs/impl/components/Water.java
@@ -0,0 +1,9 @@
+package Lesson3.drugs.impl.components;
+
+import Lesson3.drugs.Component;
+
+public class Water extends Component {
+    public Water(String name, String weight, int power) {
+        super(name, weight, power);
+    }
+}
